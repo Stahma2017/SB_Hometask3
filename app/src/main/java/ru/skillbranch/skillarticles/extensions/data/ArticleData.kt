447368aab49b0755d29076855747932e4b713aff
Stahma <StasStahma@gmail.com>
2021-08-12 19:45:45 +0300

    cleaned package structure up

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/data/ArticleData.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/data/ArticleData.kt
@@ -1,8 +1,5 @@
 package ru.skillbranch.skillarticles.extensions.data
 
-import ru.skillbranch.skillarticles.data.models.ArticleData
-import ru.skillbranch.skillarticles.data.remote.res.ArticleContentRes
-
 /*
 fun ArticleData.toArticleContentRes() : ArticleContentRes = ArticleContentRes(
     articleId = id,
